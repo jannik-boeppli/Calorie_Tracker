67b1a3c86ad57781c5b2ed57e5caa5dee6169343
jannik-boeppli <jannik.boeppli@noseryoung.ch>
2022-06-14 14:54:04 +0200

    added frontend and backend project, added erm and erd

---
+++ calorie_tracker_be/src/test/java/com/accenture/ch/calorie_tracker/CalorieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.ch.calorie_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
