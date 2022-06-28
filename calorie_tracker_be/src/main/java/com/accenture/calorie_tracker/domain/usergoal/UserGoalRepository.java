dac9c733a04a719322ae70a641337090d6b451bc
Akbar-Noser <Oliver.Pettersson@noseryoung.ch>
2022-06-28 11:02:39 +0200

    added date manipulation

--- calorie_tracker_be/src/main/java/com/accenture/calorie_tracker/domain/usergoal/UserGoalRepository.java
+++ calorie_tracker_be/src/main/java/com/accenture/calorie_tracker/domain/usergoal/UserGoalRepository.java
@@ -1,8 +1,10 @@
 package com.accenture.calorie_tracker.domain.usergoal;
 
 import com.accenture.calorie_tracker.core.generic.AbstractEntityRepository;
+import com.accenture.calorie_tracker.domain.user.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserGoalRepository extends AbstractEntityRepository<UserGoal> {
+    UserGoal findByUserAndEndTimeIsNull(User user);
 }
--- calorie_tracker_be/src/main/java/com/accenture/calorie_tracker/domain/usergoal/UserGoalServiceImpl.java
+++ calorie_tracker_be/src/main/java/com/accenture/calorie_tracker/domain/usergoal/UserGoalServiceImpl.java
@@ -12,27 +12,40 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class UserGoalServiceImpl extends AbstractEntityServiceImpl<UserGoal> implements UserGoalService {
 
     private UserService userService;
     private GoalService goalService;
+    private final UserGoalRepository userGoalRepository;
 
     @Autowired
-    public UserGoalServiceImpl(AbstractEntityRepository<UserGoal> repository, Logger logger,UserService userService, GoalService goalService) {
+    public UserGoalServiceImpl(AbstractEntityRepository<UserGoal> repository, Logger logger, UserService userService, GoalService goalService, UserGoalRepository userGoalRepository) {
         super(repository, logger);
         this.userService = userService;
         this.goalService = goalService;
+        this.userGoalRepository = userGoalRepository;
     }
 
     @Override
     protected UserGoal preSave(UserGoal newEntity) {
+
+
         //get user
         User user = newEntity.getUser();
         if (user.getId() != null) user = userService.findById(user.getId().toString());
         else if (user.getUsername() != null) user = userService.findByUsername(user.getUsername());
         if (user == null) throw new NotFoundException("User could not be found");
         newEntity.setUser(user);
+
+        // set start_time and end_time
+        UserGoal oldGoal = userGoalRepository.findByUserAndEndTimeIsNull(user);
+        LocalDateTime now = LocalDateTime.now();
+        oldGoal.setEndTime(now);
+        updateById(oldGoal.getUser().getId().toString(), oldGoal);
+        newEntity.setStartTime(now);
 
         //get goal
         Goal goal = newEntity.getGoal();
